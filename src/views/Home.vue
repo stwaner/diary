d50473b68b44b986a3841766491f23ed47726778
stwaner <stwaner@126.com>
2020-10-21 21:57:30 +0800

    首页页面完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,17 +32,186 @@
             </div>
         </div>
       </section>
+      <section id="works">
+        <div class="row">
+          <div class="twelve columns align-center">
+              <h1>Some of our recent works.</h1>
+          </div>
+          <div id="portfolio-wrapper" class="bgrid-quarters s-bgrid-halves">
+            <div class="columns portfolio-item">
+                <div class="item-wrap">
+                <a href="portfolio.html" tppabs="https://www.freemoban.com/preview/201611132213/portfolio.html">
+                      <img alt="" src="../assets/images/portfolio/geometrics.jpg" tppabs="https://www.freemoban.com/preview/201611132213/images/portfolio/geometrics.jpg">
+                      <div class="overlay"></div>
+                      <div class="link-icon"><i class="fa fa-link"></i></div>
+                    </a>
+                <div class="portfolio-item-meta">
+                  <h5><a href="portfolio.html" tppabs="https://www.freemoban.com/preview/201611132213/portfolio.html">Geometrics</a></h5>
+                      <p>Illustration</p>
+                </div>
+                </div>
+            </div>
+              <div class="columns portfolio-item">
+                <div class="item-wrap">
+                <a href="portfolio.html" tppabs="https://www.freemoban.com/preview/201611132213/portfolio.html">
+                      <img alt="" src="../assets/images/portfolio/console.jpg" tppabs="https://www.freemoban.com/preview/201611132213/images/portfolio/console.jpg">
+                      <div class="overlay"></div>
+                      <div class="link-icon"><i class="fa fa-link"></i></div>
+                    </a>
+                <div class="portfolio-item-meta">
+                  <h5><a href="portfolio.html" tppabs="https://www.freemoban.com/preview/201611132213/portfolio.html">Console</a></h5>
+                      <p>Web Development</p>
+                </div>
+                </div>
+            </div>
+              <div class="columns portfolio-item s-first">
+                <div class="item-wrap">
+                <a href="portfolio.html" tppabs="https://www.freemoban.com/preview/201611132213/portfolio.html">
+                      <img alt="" src="../assets/images/portfolio/camera-man.jpg" tppabs="https://www.freemoban.com/preview/201611132213/images/portfolio/camera-man.jpg">
+                      <div class="overlay"></div>
+                      <div class="link-icon"><i class="fa fa-link"></i></div>
+                    </a>
+                <div class="portfolio-item-meta">
+                  <h5><a href="portfolio.html" tppabs="https://www.freemoban.com/preview/201611132213/portfolio.html">Camera Man</a></h5>
+                      <p>Photography</p>
+                </div>
+                </div>
+            </div>
+              <div class="columns portfolio-item">
+                <div class="item-wrap">
+                <a href="portfolio.html" tppabs="https://www.freemoban.com/preview/201611132213/portfolio.html">
+                      <img alt="" src="../assets/images/portfolio/into-the-light.jpg" tppabs="https://www.freemoban.com/preview/201611132213/images/portfolio/into-the-light.jpg">
+                      <div class="overlay"></div>
+                      <div class="link-icon"><i class="fa fa-link"></i></div>
+                    </a>
+                <div class="portfolio-item-meta">
+                  <h5><a href="portfolio.html" tppabs="https://www.freemoban.com/preview/201611132213/portfolio.html">Into The Light</a></h5>
+                      <p>Branding</p>
+                </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="journal">
+        <div class="row">
+          <div class="twelve columns align-center">
+              <h1>Our latest posts and rants.</h1>
+          </div>
+        </div>
+
+        <div class="blog-entries">
+
+          <!-- Entry -->
+          <article class="row entry">
+
+              <div class="entry-header">
+
+                <div class="permalink">
+                    <a href="single.html" tppabs="https://www.freemoban.com/preview/201611132213/single.html"><i class="fa fa-link"></i></a>
+                </div>
+
+                <div class="ten columns entry-title pull-right">
+                    <h3><a href="single.html" tppabs="https://www.freemoban.com/preview/201611132213/single.html">Proin gravida nibh vel velit auctor aliquet Aenean sollicitudin auctor.</a></h3>
+                </div>
+
+                <div class="two columns post-meta end">
+                    <p>
+                    <time datetime="2014-01-31" class="post-date" pubdate="">Jan 31, 2014</time>
+                    <span class="dauthor">By Sakura Haruno</span>
+                    </p>
+                </div>
+
+              </div>
+
+              <div class="ten columns offset-2 post-content">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate.
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.
+                <a class="more-link" href="single.html" tppabs="https://www.freemoban.com/preview/201611132213/single.html">Read More<i class="fa fa-arrow-circle-o-right"></i></a></p>
+              </div>
+
+          </article> <!-- Entry End -->
+
+          <!-- Entry -->
+          <article class="row entry">
+
+              <div class="entry-header">
+
+                <div class="permalink">
+                    <a href="single.html" tppabs="https://www.freemoban.com/preview/201611132213/single.html"><i class="fa fa-link"></i></a>
+                </div>
+
+                <div class="ten columns entry-title pull-right">
+                    <h3><a href="single.html" tppabs="https://www.freemoban.com/preview/201611132213/single.html">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed.</a></h3>
+                </div>
+
+                <div class="two columns post-meta end">
+                    <p>
+                    <time datetime="2014-01-29" class="post-date" pubdate="">Jan 30, 2014</time>
+                    <span class="dauthor">By John Doe</span>
+                    </p>
+                </div>
+
+              </div>
+
+              <div class="ten columns offset-2 post-content">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate.
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.
+                <a class="more-link" href="single.html" tppabs="https://www.freemoban.com/preview/201611132213/single.html">Read More<i class="fa fa-arrow-circle-o-right"></i></a></p>
+              </div>
+
+          </article> <!-- Entry End -->
+
+          <!-- Entry -->
+          <article class="row entry">
+
+              <div class="entry-header">
+
+                <div class="permalink">
+                    <a href="single.html" tppabs="https://www.freemoban.com/preview/201611132213/single.html"><i class="fa fa-link"></i></a>
+                </div>
+
+                <div class="ten columns entry-title pull-right">
+                    <h3><a href="javascript:if(confirm('https://www.freemoban.com/preview/201611132213/blog-single.html  \n\n¸ÎļþΞ·¨Ӄ Teleport Ultra ςԘ, ҲΪ ·þαƷ±¨¸渃΄¼þδՒµ½¡£  \n\nģϫԚ·þαƷɏ´򿪋ü?'))window.location='https://www.freemoban.com/preview/201611132213/blog-single.html'" tppabs="https://www.freemoban.com/preview/201611132213/blog-single.html">Quis autem vel esse eum iure reprehenderit qui in ea voluptate velit esse.</a></h3>
+                </div>
+
+                <div class="two columns post-meta end">
+                    <p>
+                    <time datetime="2014-01-28" class="post-date" pubdate="">Jan 28, 2014</time>
+                    <span class="dauthor">By Naruto Uzumaki</span>
+                    </p>
+                </div>
+
+              </div>
+
+              <div class="ten columns offset-2 post-content">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate.
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.
+                <a class="more-link" href="single.html" tppabs="https://www.freemoban.com/preview/201611132213/single.html">Read More<i class="fa fa-arrow-circle-o-right"></i></a></p>
+              </div>
+
+          </article> <!-- Entry End -->
+
+        </div> <!-- Entries End -->
+
+      </section>
     </div>
+    <page-footer></page-footer>
   </div>
 </template>
 
 <script>
 import pageHeader from '../components/Header.vue'
+import pageFooter from '../components/Footer.vue'
 
 export default {
   name: 'Home',
   components: {
-    pageHeader
+    pageHeader,
+    pageFooter
   }
 }
 </script>
