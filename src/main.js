c9299a52acd74d3cdcb6655d1acb92fc1895dab0
stwaner <stwaner@126.com>
2020-12-02 18:43:51 +0800

    引入vue-amap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-// import VueAMap from 'vue-amap'
+import VueAMap from 'vue-amap'
 import App from './App.vue'
 import axios from 'axios'
 import VueRouter from 'vue-router'
@@ -20,7 +20,13 @@ Vue.prototype.$axios = axios
 console.log(process.env.VUE_APP_BASE_URL)
 Vue.use(VueRouter)
 Vue.use(ElementUI)
-// Vue.use(VueAMap)
+Vue.use(VueAMap)
+VueAMap.initAMapApiLoader({
+  key: '8d168cf9262841cf32d4e72fbfd49c49',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  // 默认高德 sdk 版本为 1.4.4
+  v: '1.4.4'
+})
 
 new Vue({
   router,
--- src/views/travels/map.vue
+++ src/views/travels/map.vue
@@ -1,42 +1,90 @@
 <template>
   <div>
     <!-- 8d168cf9262841cf32d4e72fbfd49c49 -->
-    <h3 class="title">{{ msg }}</h3>
     <div class="amap-page-container">
-      <!-- <el-amap vid="amapDemo"  :center="center" :map-manager="amapManager" :zoom="zoom" :events="events" class="amap-demo"> -->
-      <!-- </el-amap> -->
+      <el-amap ref="map" vid="amapDemo" :amap-manager="amapManager" :center="center" :zoom="zoom" :plugin="plugin" :events="events" class="amap-demo">
+        <!-- <el-amap-marker vid="component-marker" :position="componentMarker.position" :content-render="componentMarker.contentRender" ></el-amap-marker> -->
+        <el-amap-marker v-for="(marker, index) in markers" :key="index" :position="marker.position" :events="marker.events" :visible="marker.visible" :draggable="marker.draggable" :vid="index"></el-amap-marker>
+      </el-amap>
+      <div class="toolbar">
+        <button @click="getMap()">get map</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-// let amapManager = new VueAMap.AMapManager();
+import VueAMap from 'vue-amap'
+const amapManager = new VueAMap.AMapManager()
 export default {
+  name: 'demo',
   data () {
     return {
-      msg: 'vue-amap向你问好！',
-      // zoom: 12,
-      // center: [121.59996, 31.197646],
-      // amapManager,
-      // events: {
-      //   init(map) {
-      //     AMapUI.loadUI(['overlay/SimpleMarker'], function(SimpleMarker) {
-      //       const marker = new SimpleMarker({
-      //         iconLabel: 'A',
-      //         iconStyle: 'red',
-      //         map: map,
-      //         position: map.getCenter()
-      //       })
-      //     })
-      //   }
-      // }
+      amapManager,
+      zoom: 12,
+      center: [116.512782, 39.792023],
+      markers: [
+        {
+          position: [116.512782, 39.792023],
+          events: {
+            click: () => {
+              alert('click marker')
+            },
+            // 点标记拖拽移动结束触发事件
+            dragend: (e) => {
+              console.log('---event---: dragend', e.lnglat.lng, e.lnglat.lat)
+              this.markers[0].position = [e.lnglat.lng, e.lnglat.lat]
+            }
+          },
+          visible: true,
+          draggable: true,
+          template: '<span>1</span>'
+        }
+      ],
+      events: {
+        init: (o) => {
+          console.log(this.$refs.map.$$getCenter())
+          console.log(this.$refs.map.$$getInstance())
+          o.getCity(result => {
+            console.log(result)
+          })
+        },
+        moveend: () => {
+        },
+        zoomchange: () => {
+        },
+        click: (e) => {
+          alert('map clicked')
+        }
+      },
+      plugin: ['ToolBar', {
+        pName: 'MapType',
+        defaultType: 0,
+        events: {
+          init (o) {
+            console.log(o)
+          }
+        }
+      }]
+    }
+  },
+  methods: {
+    getMap () {
+      // amap vue component
+      console.log(amapManager._componentMap)
+      // gaode map instance
+      console.log(amapManager._map)
     }
   }
 }
 </script>
 
-<style>
-.amap-demo {
-  height: 300px;
+<style lang="scss">
+.amap-page-container{
+  .amap-demo {
+    width: 1000px;
+    height: 650px;
+    margin: 0 auto;
+  }
 }
 </style>
