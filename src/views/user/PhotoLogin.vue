bb5bb8cad06f4f751c277203d26e31f1c6da8a02
stwaner <stwaner@126.com>
2021-01-29 18:18:46 +0800

    1

--- src/views/user/PhotoLogin.vue
+++ src/views/user/PhotoLogin.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="tracking">
-    <video id="video" width="318" height="300" preload autoplay loop muted></video>
-    <canvas id="canvas" width="318" height="270" ></canvas>
-    <div style="text-align:center;">
-      <img :src="dataUrl" alt="" id="img1">
-    </div>
-    <div class="buttonDiv">
-      <el-button type="primary" @click="openCamera">重新拍照</el-button>
-    </div>
+      <video id="video" width="318" height="270" preload autoplay loop muted></video>
+      <canvas id="canvas" width="318" height="270" ></canvas>
+      <div style="text-align:center;">
+        <img :src="dataUrl" alt="" id="img1">
+      </div>
+      <div class="buttonDiv">
+        <el-button type="primary" @click="openCamera">重新拍照</el-button>
+      </div>
     x: {{x}} - y: {{y}}<br/>
     w: {{w}} - h: {{h}}
   </div>
@@ -24,6 +24,7 @@ export default {
       trackering: null,
       mediaStreamTrack: null,
       dataUrl: '',
+      loginSucc: false,
       x: 0,
       y: 0,
       w: 0,
@@ -36,8 +37,8 @@ export default {
   },
   methods: {
     openCamera () {
-      let tipFlag = true // 是否检测
       let faceflag = false // 是否进行拍照
+      let isCheck = true // 是否检测
       const _this = this
       var video = document.getElementById('video')
       var canvas = document.getElementById('canvas')
@@ -52,36 +53,36 @@ export default {
       // 创建监听 每帧都会触发
       tracker.on('track', function (event) {
         context.clearRect(0, 0, canvas.width, canvas.height)
-        if (tipFlag) {
+        if (isCheck) {
           if (event.data.length === 0) {
             // 未检测到人脸
-            context.fillText('未检测到人脸', 150, 15)
+            context.fillText('未检测到人脸，请对准摄像头', 150, 15)
           } else if (event.data.length === 1) { // 长度为多少代表检测到几张人脸
-            // 检测到一张人脸
             // 给检测到的人脸绘制矩形
             event.data.forEach(function (rect) {
               faceflag = true
+              context.strokeStyle = '#a64ceb'
               context.font = '11px Helvetica'
-              context.fillText('已识别到人脸', 150, 15)
-              context.strokeStyle = '#ff4d4f'
               context.strokeRect(rect.x, rect.y, rect.width, rect.height) // 绘制矩形
+              context.fillText('已识别到人脸', 150, 15)
+              // context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11)
+              // context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22)
               _this.x = rect.x
               _this.y = rect.y
               _this.w = rect.width
               _this.h = rect.height
             })
-            if (faceflag) { // 检测到人脸进行拍照，延迟两秒
-              tipFlag = false
+            if (faceflag) { // 检测到人脸进行拍照，延迟1秒
+              isCheck = false
               faceflag = false
               _this.$message.info('正在拍照，请勿乱动~')
               setTimeout(() => {
                 _this.submit() // 拍照
-              }, 2000)
+              }, 1000)
             }
           } else {
             // 检测到多张人脸
             _this.$message.info('只可一人进行人脸识别！')
-            return
           }
         }
       })
@@ -91,103 +92,40 @@ export default {
       const canvas = document.getElementById('canvas')
       const context = canvas.getContext('2d')
       const video = document.getElementById('video')
-      setTimeout(function () {
-        // context.drawImage(video, (that.x + that.w), (that.y + that.h), (that.x + that.w), (that.y + that.h), (that.x), that.y, (that.w), (that.h))
-        context.drawImage(video, (that.x + that.w), (that.y + that.h),  (that.x + that.w), (that.y + that.h), 0, 0,  (that.x + that.w), (that.y + that.h))
-        // context.drawImage(video, that.x, that.y,  120, 120, 0, 0, 120, 120)
-        const dataUrl = canvas.toDataURL('image/png')
-        console.log('dataUrl----', dataUrl)
-        that.dataUrl = dataUrl
-      }, 1000)
-      // context.drawImage(video, 0, 0, 500, 400)
-      canvas.toBlob((blob) => {
-        console.log(blob)
-        // axios.post({ faceUrl: URL.createObjectURL(blob) }).then(res => {
-        //   let type = 'success'
-        //   // 登录成功跳转到首页
-        //   if (res.data.code === 200) {
-        //     that.$router.push({ path: '/' })
-        //   }
-        //   // 登录失败重新进行人脸检测
-        //   else {
-        //     type = 'error'
-        //     // setTimeout(() => {
-        //     //   flag = true
-        //     // }, 1500)
-        //   }
-        //   that.$message({
-        //     message: res.data.message,
-        //     type,
-        //     center: true
-        //   })
-        // })
-      })
+      // setTimeout(function () {
+      // context.drawImage(video, (that.x + that.w), (that.y + that.h), (that.x + that.w), (that.y + that.h), (that.x), that.y, (that.w), (that.h))
+      context.drawImage(video, (that.x + that.w), (that.y + that.h), (that.x + that.w), (that.y + that.h), 0, 0, that.x, that.y)
+      const dataUrl = canvas.toDataURL('image/png')
+      that.dataUrl = dataUrl
+      console.log(dataUrl)
+      // }, 1000)
+      // that.$store.dispatch('LoginByFaceID', { face_img: dataURL.slice(23) }).then(res => {
+      //   let type = 'success'
+      //   if (res.code === 200) {
+      //     that.$router.push({ path: '/' })
+      //   } else { // 登录失败重新进行人脸检测
+      //     type = 'error'
+      //     setTimeout(() => {
+      //       that.isCheck = false
+      //       that.faceflag = true
+      //     }, 1500)
+      //   }
+      //   that.$message({
+      //     message: res.message,
+      //     type,
+      //     center: true
+      //   })
+      // }).catch(error => {
+      //   console.log(error)
+      // })
+      // canvas.toBlob((blob) => {
+      // console.log(blob)
+      // axios.post({ faceUrl: URL.createObjectURL(blob) }).then(res => {
+      // })
+    // })
     },
-    // getCompetence () {
-    //   let flag = true
-    //   const _this = this
-    //   const video = this.mediaStreamTrack = document.getElementById('videoCamera-face')
-    //   const canvas = document.getElementById('canvas')
-    //   const context = canvas.getContext('2d')
-
-    //   const tracker = new window.tracking.ObjectTracker('face')
-    //   tracker.setInitialScale(4)
-    //   tracker.setStepSize(2)
-    //   tracker.setEdgesDensity(0.1)
-    //   // 启动摄像头初始化
-    //   this.trackerTask = tracking.track('#videoCamera-face', tracker, {
-    //     camera: true
-    //   })
-    //   tracker.on('track', function (event) {
-    //     console.log(event)
-    //     context.clearRect(0, 0, canvas.width, canvas.height)
-    //     event.data.forEach(function (rect) {
-    //       context.strokeStyle = '#ff0000'
-    //       context.strokeRect(rect.x, rect.y, rect.width, rect.height)
-    //     })
-    //     if (event.data.length) {
-    //       // 会不停的去检测人脸，所以这里需要做个锁
-    //       if (flag) {
-    //         // 裁剪出人脸并绘制下来
-    //         const canvasUpload = document.getElementById('canvas-face-upload')
-    //         const contextUpload = canvasUpload.getContext('2d')
-    //         contextUpload.drawImage(video, 0, 0, 105, 105)
-    //         flag = false
-    //         // 人脸的basa64
-    //         const dataURL = canvasUpload.toDataURL('image/jpeg')
-    //         console.log(dataURL)
-    //         // ajax请求
-    //         // _this.$store.dispatch('LoginByFaceID', {
-    //         //     face_img: dataURL.slice(23) // 我们后台需要的basa64不要前缀
-    //         // }).then(res => {
-    //         //     let type = 'success'
-    //         //     // 登录成功跳转到首页
-    //         //     if (res.data.code === 200) {
-    //         //         _this.$router.push({
-    //         //             path: '/'
-    //         //         });
-    //         //     }
-    //         //     // 登录失败重新进行人脸检测
-    //         //     else {
-    //         //         type = 'error'
-    //         //         setTimeout(() => {
-    //         //             flag = true
-    //         //         }, 1500)
-    //         //     }
-    //         //     _this.$message({
-    //         //         message: res.data.message,
-    //         //         type,
-    //         //         center: true
-    //         //     });
-    //         // }).catch(error => {
-    //         //     console.log(error)
-    //         // })
-    //       }
-    //     }
-    //   })
-    // },
     destroyed () {
-      // console.log('销毁')
+      console.log('销毁')
       // // 停止侦测
       // this.trackerTask.stop()
       // // 关闭摄像头
@@ -216,9 +154,9 @@ export default {
   }
   img{
     display: inline-block;
-    max-width: 120px;
+    // max-width: 120px;
     margin-bottom: 20px;
-    border: 1px solid #ededed;
+    // border: 1px solid #ededed;
   }
 }
 </style>
